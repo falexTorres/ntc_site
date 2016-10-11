8f50637afcbbd477a43549cb5bfa4bc6bbe30e0f
root <root@localhost.localdomain>
2016-10-11 14:50:43 -0500

    progress on splash

--- app/controllers/front_end_controller.rb
+++ app/controllers/front_end_controller.rb
@@ -1,4 +1,19 @@
 class FrontEndController < ApplicationController
+  layout :resolve_layout
+
   def home
   end
+
+  def index
+  end
+
+  private
+    def resolve_layout
+      case action_name
+        when "home"
+          "application"
+        else
+          "index"
+       end
+    end
 end
